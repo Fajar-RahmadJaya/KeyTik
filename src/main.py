94065e385b29b515300a8dcc7c60b44ec567f9fa
Fajar Rahmad Jaya <Fajar.Rahmad.Jaya.Profile@gmail.com>
2026-03-31 15:28:23 +0700

    refactor: use announcement composition

--- src/main.py
+++ src/main.py
@@ -6,7 +6,7 @@ from PySide6.QtWidgets import (QApplication) # pylint: disable=E0611
 import utility.utils as utils
 import utility.thread as thread
 from dashboard.dashborad import Dashboard
-
+from setting.announcement import Announcement
 
 def main():
     "Main function"
@@ -17,13 +17,14 @@ def main():
 
     app = QApplication(sys.argv)
     main_window = Dashboard()
+    announcement = Announcement()
     main_window.show()
 
     main_window.startup_worker = thread.Thread(main_window)
     main_window.startup_worker.update_found.connect(
         main_window.show_update_messagebox)
     main_window.startup_worker.show_announcement.connect(
-        main_window.show_announcement_window)
+        announcement.show_announcement_window)
     main_window.startup_worker.start()
 
     sys.exit(app.exec())

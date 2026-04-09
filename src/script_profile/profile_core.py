d8db427c764cf138d2c184dee0f33f6bebcbe1d7
Fajar Rahmad Jaya <Fajar.Rahmad.Jaya.Profile@gmail.com>
2026-04-09 22:00:39 +0700

    refactor: divide key_listening

--- src/script_profile/profile_core.py
+++ src/script_profile/profile_core.py
@@ -10,21 +10,6 @@ from PySide6.QtCore import QTimer, Signal, QObject, QEvent  # pylint: disable=E0
 from utility import constant
 
 
-
-class InputBlocker(QObject):
-    "Input blocker"
-    def event_filter(self, _, event):
-        "Filter event by key press and window"
-        if event.type() in (QEvent.MouseButtonPress, QEvent.MouseButtonRelease,
-                            QEvent.KeyPress, QEvent.KeyRelease,
-                            QEvent.FocusIn, QEvent.FocusOut):
-            return True
-        if event.type() in (QEvent.Close, QEvent.WindowDeactivate,
-                            QEvent.Hide, QEvent.Leave):
-            return True
-        return False
-
-
 class ProfileCore(QObject):
     "Create/edit profile logic"
     request_timer_start = Signal(object)
@@ -35,8 +20,6 @@ class ProfileCore(QObject):
 
         self.is_listening = False
         self.use_scan_code = False
-        self._window_blocker = InputBlocker()
-        self._input_blocker = InputBlocker()
         self.pressed_keys = []
         self.pressed_keys = []
         self.last_combination = ""
@@ -44,6 +27,17 @@ class ProfileCore(QObject):
         self.active_entry = None
         self.previous_button_text = None
         self.set_timer = None
+
+    def eventFilter(self, _, event):  # pylint: disable=C0103
+        "Filter event by key press and window"
+        if event.type() in (QEvent.MouseButtonPress, QEvent.MouseButtonRelease,
+                            QEvent.KeyPress, QEvent.KeyRelease,
+                            QEvent.FocusIn, QEvent.FocusOut):
+            return True
+        if event.type() in (QEvent.Close, QEvent.WindowDeactivate,
+                            QEvent.Hide, QEvent.Leave):
+            return True
+        return False
 
     def update_entry(self):
         "Add + on multi key press"
@@ -55,7 +49,7 @@ class ProfileCore(QObject):
         "Get profile name from entry"
         script_name = self.script_name_entry.text().strip()
         if not script_name:
-            QMessageBox.warning(None, "Input Error", "Please enter a Profile name.") 
+            QMessageBox.warning(None, "Input Error", "Please enter a Profile name.")
             return None
 
         if not script_name.endswith('.ahk'):
@@ -73,157 +67,74 @@ class ProfileCore(QObject):
 
     def key_listening(self, entry_widget, button):
         "Get and Listen to key press"
-        def toggle_other_buttons(state):
-            if hasattr(self, 'key_rows'):
-                for key_row in self.key_rows:
-                    (_, _, orig_button, remap_button, _, _, _, _) = key_row
-
-                    if orig_button != button and orig_button is not None:
-                        orig_button.setEnabled(state)
-                    if remap_button != button and remap_button is not None:
-                        remap_button.setEnabled(state)
-
-            if hasattr(self, 'copas_rows'):
-                for copas_row in self.copas_rows:
-                    (_, _, copy_button, paste_button, _, _) = copas_row
-
-                    if copy_button != button and copy_button is not None:
-                        copy_button.setEnabled(state)
-                    if paste_button != button and paste_button is not None:
-                        paste_button.setEnabled(state)
-
-            for _, shortcut_button in self.shortcut_rows:
-                if shortcut_button != button and shortcut_button is not None:
-                    shortcut_button.setEnabled(state)
-
         if not self.is_listening:
-
             self.is_listening = True
             self.active_entry = entry_widget
             self.previous_button_text = button.text()
-
             self.use_scan_code = False
-            if hasattr(self, 'key_rows'):
-                for key_row in self.key_rows:
-                    (orig_entry, remap_entry, orig_button, _, _, _, _,
-                     hold_interval_entry) = key_row
+            self.pressed_keys = []
+            self.last_combination = ""
 
-                    if button == orig_button:
-                        parent_widget = button.parent()
-                        if parent_widget:
-                            sc_checkboxes = [child for child
-                                             in parent_widget.
-                                             findChildren(QObject)
-                                             if child.objectName() ==
-                                             "sc_checkbox"]
-                            if sc_checkboxes:
-                                self.use_scan_code = (
-                                    sc_checkboxes[0].isChecked())
-                            break
+            self.disable_input()
 
-            entries_to_disable = []
-            if hasattr(self, 'script_name_entry'):
-                entries_to_disable.append((self.script_name_entry, None))
-            if hasattr(self, 'keyboard_entry'):
-                entries_to_disable.append((self.keyboard_entry, None))
-            if hasattr(self, 'program_entry'):
-                entries_to_disable.append((self.program_entry, None))
+            self.edit_window.installEventFilter(self)
 
-            if hasattr(self, 'key_rows'):
-                for key_row in self.key_rows:
-                    (
-                        orig_entry, remap_entry, _, _, _, _, _,
-                        hold_interval_entry
-                    ) = key_row
-                    entries_to_disable.append((orig_entry, None))
-                    entries_to_disable.append((remap_entry, None))
-                    entries_to_disable.append((hold_interval_entry, None))
-
-            if hasattr(self, 'shortcut_rows'):
-                for shortcut_entry, _ in self.shortcut_rows:
-                    entries_to_disable.append((shortcut_entry, None))
-
-            if hasattr(self, 'copas_rows'):
-                for copas_row in self.copas_rows:
-                    copy_entry, paste_entry, _, _, _, _ = copas_row
-                    entries_to_disable.append((copy_entry, None))
-                    entries_to_disable.append((paste_entry, None))
-
-            self.disable_input(entries_to_disable)
-
-            if hasattr(self, "edit_window"):
-                if not hasattr(self, "_window_blocker"):
-                    self._window_blocker = InputBlocker()
-                self.edit_window.installEventFilter(self._window_blocker)
-
-            toggle_other_buttons(False)
+            self.toggle_other_buttons(False, button)
 
             button.clicked.disconnect()
             button.clicked.connect(lambda: self.key_listening
                                    (entry_widget, button))
 
-            self.pressed_keys = []
-            self.last_combination = ""
             self.set_timer = QTimer()
             self.set_timer.setSingleShot(True)
-            self.set_timer.timeout.connect(lambda:
-                                               self.finalize_combination
-                                               (entry_widget))
-            keyboard.hook(lambda event: self.multi_key_event
-                          (event, entry_widget, button))
+            self.set_timer.timeout.connect(
+                lambda: self.finalize_combination(entry_widget))
+
+            keyboard.hook(lambda event: self.multi_key_event(event, entry_widget, button))
 
         else:
             self.is_listening = False
             self.active_entry = None
             self.pressed_keys = []
 
-            entries_to_enable = []
-            if hasattr(self, 'script_name_entry'):
-                entries_to_enable.append((self.script_name_entry, None))
+            self.enable_input()
+            self.toggle_other_buttons(True, button)
 
-            if hasattr(self, 'keyboard_entry'):
-                entries_to_enable.append((self.keyboard_entry, None))
-
-            if hasattr(self, 'program_entry'):
-                entries_to_enable.append((self.program_entry, None))
-
-            if hasattr(self, 'key_rows'):
-                for key_row in self.key_rows:
-                    (
-                        orig_entry, remap_entry, _, _, _, _, _,
-                        hold_interval_entry
-                    ) = key_row
-                    entries_to_enable.append((orig_entry, None))
-                    entries_to_enable.append((remap_entry, None))
-                    entries_to_enable.append((hold_interval_entry, None))
-
-            if hasattr(self, 'shortcut_rows'):
-                for shortcut_entry, _ in self.shortcut_rows:
-                    entries_to_enable.append((shortcut_entry, None))
-
-            if hasattr(self, 'copas_rows'):
-                for copas_row in self.copas_rows:
-                    copy_entry, paste_entry, _, _, _, _ = copas_row
-                    entries_to_enable.append((copy_entry, None))
-                    entries_to_enable.append((paste_entry, None))
-
-            self.enable_input(entries_to_enable)
-            toggle_other_buttons(True)
-
-            if hasattr(self, "edit_window") and hasattr(
-                    self, "_window_blocker"):
-                self.edit_window.removeEventFilter(self._window_blocker)
+            self.edit_window.removeEventFilter(self)
 
             if button is not None:
                 button.clicked.disconnect()
                 button.clicked.connect(lambda: self.key_listening
                                        (entry_widget, button))
 
+    def toggle_other_buttons(self, state, button):
+        "Change the state of non selected button"
+        for key_row in self.key_rows:
+            (_, _, orig_button, remap_button, _, _, _, _) = key_row
+
+            if orig_button != button and orig_button is not None:
+                orig_button.setEnabled(state)
+            if remap_button != button and remap_button is not None:
+                remap_button.setEnabled(state)
+
+        for copas_row in self.copas_rows:
+            (_, _, copy_button, paste_button, _, _) = copas_row
+
+            if copy_button != button and copy_button is not None:
+                copy_button.setEnabled(state)
+            if paste_button != button and paste_button is not None:
+                paste_button.setEnabled(state)
+
+        for _, shortcut_button in self.shortcut_rows:
+            if shortcut_button != button and shortcut_button is not None:
+                shortcut_button.setEnabled(state)
+
     def multi_key_event(self, event, entry_widget, button):
         "Action when multiple key is pressed, set timer before saving the key"
         if not self.is_listening or self.active_entry != entry_widget:
             return
 
+        self.use_scan_code = self.handle_sc_listening(button)
         if hasattr(self, 'use_scan_code') and self.use_scan_code:
             key = f"SC{event.scan_code:02X}"
         else:
@@ -254,6 +165,25 @@ class ProfileCore(QObject):
                 else:
                     if hasattr(self, "release_timer"):
                         self.request_timer_start.emit(entry_widget)
+
+    def handle_sc_listening(self, button):
+        "Check whether to use scan code listening or not"
+        for key_row in self.key_rows:
+            (_, _, orig_button, _, _, _, _,_) = key_row
+
+            if button == orig_button:
+                parent_widget = button.parent()
+                if parent_widget:
+                    sc_checkboxes = [child for child
+                                        in parent_widget.
+                                        findChildren(QObject)
+                                        if child.objectName() ==
+                                        "sc_checkbox"]
+                    if sc_checkboxes:
+                        self.use_scan_code = sc_checkboxes[0].isChecked()
+                        return self.use_scan_code
+                    break
+        return None
 
     def mouse_listening(self, button, pressed):
         "Get and listen to mouse key press"
@@ -319,22 +249,64 @@ class ProfileCore(QObject):
         entry_widget.setText(self.last_combination)
         self.pressed_keys = []
 
-    def disable_input(self, entry_rows):
+    def disable_input(self):
         "Disable input. Used on key listening"
-        if not hasattr(self, "_input_blocker"):
-            self._input_blocker = InputBlocker()
-        for entry_tuple in entry_rows:
+        entries_to_disable = []
+
+        entries_to_disable.append((self.script_name_entry, None))
+        entries_to_disable.append((self.keyboard_entry, None))
+        entries_to_disable.append((self.program_entry, None))
+
+        for key_row in self.key_rows:
+            (orig_entry, remap_entry,
+                _, _, _, _, _,hold_interval_entry) = key_row
+            entries_to_disable.append((orig_entry, None))
+            entries_to_disable.append((remap_entry, None))
+            entries_to_disable.append((hold_interval_entry, None))
+
+        for shortcut_entry, _ in self.shortcut_rows:
+            entries_to_disable.append((shortcut_entry, None))
+
+        # Part of pro version code
+        for copas_row in self.copas_rows:
+            copy_entry, paste_entry, _, _, _, _ = copas_row
+            entries_to_disable.append((copy_entry, None))
+            entries_to_disable.append((paste_entry, None))
+
+        for entry_tuple in entries_to_disable:
             entry = entry_tuple[0]
             if entry is not None:
-                entry.installEventFilter(self._input_blocker)
+                entry.installEventFilter(self)
 
-    def enable_input(self, entry_rows):
+    def enable_input(self):
         "Enable input. Used on key listening"
-        if hasattr(self, "_input_blocker"):
-            for entry_tuple in entry_rows:
-                entry = entry_tuple[0]
-                if entry is not None:
-                    entry.removeEventFilter(self._input_blocker)
+        entries_to_enable = []
+
+        entries_to_enable.append((self.script_name_entry, None))
+        entries_to_enable.append((self.keyboard_entry, None))
+        entries_to_enable.append((self.program_entry, None))
+
+        for key_row in self.key_rows:
+            (
+                orig_entry, remap_entry, _, _, _, _, _,
+                hold_interval_entry
+            ) = key_row
+            entries_to_enable.append((orig_entry, None))
+            entries_to_enable.append((remap_entry, None))
+            entries_to_enable.append((hold_interval_entry, None))
+
+        for shortcut_entry, _ in self.shortcut_rows:
+            entries_to_enable.append((shortcut_entry, None))
+
+        for copas_row in self.copas_rows:
+            copy_entry, paste_entry, _, _, _, _ = copas_row
+            entries_to_enable.append((copy_entry, None))
+            entries_to_enable.append((paste_entry, None))
+
+        for entry_tuple in entries_to_enable:
+            entry = entry_tuple[0]
+            if entry is not None:
+                entry.removeEventFilter(self)
 
     def load_key_list(self):
         "Load list of hard coded key from json file"
@@ -352,7 +324,7 @@ class ProfileCore(QObject):
         except FileNotFoundError as e:
             print(f"Error reading key list: {e}")
         return key_map
-    
+
     def load_key_translations(self):
         "Load translation from raw key to readable key"
         key_translations = {}

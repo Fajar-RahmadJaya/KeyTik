bb7068b7e6a3112fcebf7c627e1d3ef73b598511
Fajar Rahmad Jaya <Fajar.Rahmad.Jaya.Profile@gmail.com>
2026-04-11 00:12:37 +0700

    refactor: move method from profile ui

--- src/script_profile/remap_row.py
+++ src/script_profile/remap_row.py
@@ -2,7 +2,7 @@
 
 from PySide6.QtWidgets import (  # pylint: disable=E0611
     QLabel, QPushButton, QCheckBox, QLineEdit, QFrame, QHBoxLayout,
-    QVBoxLayout, QWidget, QSizePolicy, QGridLayout, QTextEdit
+    QVBoxLayout, QWidget, QSizePolicy, QGridLayout, QTextEdit, QSpacerItem
 )
 from PySide6.QtCore import Qt  # pylint: disable=E0611
 from PySide6.QtSvgWidgets import QSvgWidget  # pylint: disable=E0611
@@ -17,14 +17,126 @@ class RemapRow(ParseScript, ProfileCore):
     def __init__(self, edit_frame):
         super().__init__()
         self.edit_frame = edit_frame
+
+        # Variables
         self.mapping_row_widgets = []
         self.shortcut_row_widgets = []
+        self.key_rows = []
+        self.shortcut_rows = []
+        self.files_opener_rows = []
+        self.files_opener_row_widgets = []
         self.row_num = 0
 
+        # UI
         self.edit_frame_layout = None
         self.text_block = None
         self.shortcut_entry = None
         self.is_text_mode = None
+
+    def handle_mode_changed(self, index):
+        "Action when mode changed from combobox (can be moved)"
+        while self.edit_frame_layout.count():
+            item = self.edit_frame_layout.takeAt(0)
+            widget = item.widget()
+            if widget:
+                widget.setParent(None)
+
+        self.key_rows = []
+        self.shortcut_rows = []
+        if hasattr(self, "files_opener_rows"):
+            self.files_opener_rows = []
+        if hasattr(self, "files_opener_row_widgets"):
+            self.files_opener_row_widgets = []
+        if hasattr(self, "text_block"):
+            self.text_block = None
+        self.is_text_mode = False
+
+        if index == 0:
+            self.is_text_mode = False
+            self.shortcut_title()
+            self.shortcut_row()
+            self.remap_title()
+            self.remap_row()
+            self.edit_frame_layout.addItem(QSpacerItem(20, 40,
+                                            QSizePolicy.Minimum,
+                                            QSizePolicy.Expanding))
+
+        elif index == 1:
+            self.is_text_mode = True
+            self.shortcut_title()
+            self.shortcut_row()
+            self.edit_frame_layout.addItem(QSpacerItem(20, 40,
+                                            QSizePolicy.Minimum,
+                                            QSizePolicy.Expanding))
+
+        else:
+            self.pro_mode(index)
+
+    def handle_parser(self, lines, first_line):
+        "Action when editing profile (Can be moved)"
+        key_map = self.load_key_list()
+        mode_line = lines[0].strip() if lines else "; default"
+
+        shortcuts = []
+        remaps = []
+
+        if mode_line == "; default":
+            shortcuts, remaps = self.parse_default_mode(lines, key_map)
+
+            self.shortcut_title()
+
+            if not shortcuts:
+                self.shortcut_row()
+            else:
+                for shortcut in shortcuts:
+                    self.shortcut_row(shortcut)
+
+            self.remap_title()
+
+            if not remaps:
+                self.remap_row()
+            else:
+                for (default_key, remap_key, is_text_format,
+                        is_hold_format, hold_interval, is_first_key,
+                        is_sc) in remaps:
+                    self.remap_row(
+                        default_key,
+                        remap_key,
+                        is_text_format=is_text_format,
+                        is_hold_format=is_hold_format,
+                        hold_interval=hold_interval,
+                        is_first_key=is_first_key,
+                        is_sc=is_sc
+                    )
+
+            self.update_plus_visibility('shortcut')
+            self.update_plus_visibility('remap')
+
+        elif mode_line == "; text":
+            self.is_text_mode = True
+            self.text_block = QTextEdit(self.edit_frame)
+            self.text_block.setLineWrapMode(QTextEdit.WidgetWidth)
+            self.text_block.setFixedHeight(14 * self.text_block.fontMetrics().height())
+            self.text_block.setFontPointSize(10)
+            self.edit_frame_layout.addWidget(self.text_block)
+
+            shortcuts = self.parse_shortcuts(lines, key_map)
+
+            self.row_num += 1
+
+            text_content = self.extract_and_filter_content(lines)
+            self.text_block.setPlainText(text_content.strip())
+
+            if not shortcuts:
+                self.shortcut_row()
+            else:
+                for shortcut in shortcuts:
+                    self.shortcut_row(shortcut)
+
+            self.update_plus_visibility('shortcut')
+
+        else:
+            self.pro_parser(lines, first_line)
 
     def remap_row(self, default_key='', remap_key='', insert_after=None,
                   is_text_format=False, is_hold_format=False,
@@ -543,69 +655,3 @@ class RemapRow(ParseScript, ProfileCore):
                 left_sep.setVisible(is_last)
             if right_sep:
                 right_sep.setVisible(is_last)
-
-    def handle_parser(self, lines, first_line):
-        "Action when editing profile (Can be moved)"
-        key_map = self.load_key_list()
-        mode_line = lines[0].strip() if lines else "; default"
-
-        shortcuts = []
-        remaps = []
-
-        if mode_line == "; default":
-            shortcuts, remaps = self.parse_default_mode(lines, key_map)
-
-            self.shortcut_title()
-
-            if not shortcuts:
-                self.shortcut_row()
-            else:
-                for shortcut in shortcuts:
-                    self.shortcut_row(shortcut)
-
-            self.remap_title()
-
-            if not remaps:
-                self.remap_row()
-            else:
-                for (default_key, remap_key, is_text_format,
-                        is_hold_format, hold_interval, is_first_key,
-                        is_sc) in remaps:
-                    self.remap_row(
-                        default_key,
-                        remap_key,
-                        is_text_format=is_text_format,
-                        is_hold_format=is_hold_format,
-                        hold_interval=hold_interval,
-                        is_first_key=is_first_key,
-                        is_sc=is_sc
-                    )
-
-            self.update_plus_visibility('shortcut')
-            self.update_plus_visibility('remap')
-
-        elif mode_line == "; text":
-            self.is_text_mode = True
-            self.text_block = QTextEdit(self.edit_frame)
-            self.text_block.setLineWrapMode(QTextEdit.WidgetWidth)
-            self.text_block.setFixedHeight(14 * self.text_block.fontMetrics().height())
-            self.text_block.setFontPointSize(10)
-            self.edit_frame_layout.addWidget(self.text_block)
-
-            shortcuts = self.parse_shortcuts(lines, key_map)
-
-            self.row_num += 1
-
-            text_content = self.extract_and_filter_content(lines)
-            self.text_block.setPlainText(text_content.strip())
-
-            if not shortcuts:
-                self.shortcut_row()
-            else:
-                for shortcut in shortcuts:
-                    self.shortcut_row(shortcut)
-
-            self.update_plus_visibility('shortcut')
-
-        else:
-            self.pro_parser(lines, first_line)
